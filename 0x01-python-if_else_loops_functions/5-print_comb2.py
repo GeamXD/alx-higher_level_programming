90dc77ad0f00107e5e0c72b8afc8cb4e2a1b8f90
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-20 16:02:33 +0100

    py files

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(100):
     first_digit = i // 10
     last_digit = i % 10
     if i < 99:
-        print(f"{first_digit:d}{last_digit:d}", end=", ")
+        print("{}{}".format(first_digit, last_digit), end=", ")
     else:
-        print(f"{first_digit:d}{last_digit:d}")
+        print("{}{}".format(first_digit, last_digit))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for num1 in range(10):
     for num2 in range(10):
         if num1 < num2 and num1 != num2:
-            print(f"{num1}{num2}", end="")
+            print("{}{}".format(num1, num2), end="")
             if num1 != 8:
                 print(end=", ")
             if num1 == 8:
