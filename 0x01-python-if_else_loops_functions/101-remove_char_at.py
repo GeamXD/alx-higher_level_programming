5bd6ec86adba280fcb3029d2797c456a4eced40f
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-20 17:00:48 +0100

    py files

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    output = ""
+    count = 0
+    for c in str:
+        if c:
+            if count == n:
+                pass
+            else:
+                output += c
+        count += 1
+    return output
