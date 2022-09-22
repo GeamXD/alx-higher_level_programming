636ffad784eef5ddde5d23e356cb04711b18e3bc
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-22 13:07:12 +0100

    py files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     count = 0
     for arg in argv:
-        if count == 0 and len(argv) - 1 == 0:
+        if len(argv) == 1:
             print("{} arguments.".format(len(argv) - 1))
-        elif count == 0:
+        elif count == 0 and len(argv) > 1:
             print("{} arguments:".format(len(argv) - 1))
-        else:
+        elif len(argv) > 1 and count > 0:
             print("{}: {}".format(count, arg))
         count += 1
