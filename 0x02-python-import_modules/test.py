0cbbdfa852e9255fa08b446201532363830d17c4
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-23 14:49:20 +0100

    py files

--- 0x02-python-import_modules/test.py
+++ 0x02-python-import_modules/test.py
@@ -3,13 +3,7 @@ from sys import argv
 res = 0
 count = 0
 for arg in argv:
-    if len(argv) == 0:
-        pass
-    elif len(argv) > 0 and count > 0:
-        res += int(arg)
-    count += 1
-print(res)
-
+    print(arg)
 #res = 0
 #nums = [1]
 #for i in nums:
