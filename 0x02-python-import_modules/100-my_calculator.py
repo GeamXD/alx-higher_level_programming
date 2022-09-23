df6b56e5cec1f9558f0a4511cf8df356d3da053e
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-23 15:41:06 +0100

    py files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,17 @@
 if __name__ == "__main__":
     from calculator_1 import div, mul, sub, add
     from sys import argv, exit
-    operator = ["+", "-", "/", "*"]
-    operator2 = ["/", "-"]
-    operator3 = ["+", "*"]
-    count = len(argv) - 1
-    if count != 3:
+
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
         exit(1)
-    sign = argv[2]
-    if sign not in operator:
+
+    operator = {"+": add, "-": sub, "/": div, "*": mul}
+
+    if argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
-    if sign in operator3:
-        if sign == "+":
-            print("{} {} {} = {}".format(a, sign, b, add(a, b)))
-        else:
-            print("{} {} {} = {}".format(a, sign, b, mul(a, b)))
-    if sign in operator2:
-        if sign == "-":
-            print("{} {} {} = {}".format(a, sign, b, sub(a, b)))
-        else:
-            print("{} {} {} = {}".format(a, sign, b, div(a, b)))
+    print("{} {} {} = {}".format(a, argv[2], b, operator[argv[2]](a, b)))
