6683b49f4ec773611773dad92103bb7d69be2752
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-23 15:01:07 +0100

    py files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import div, mul, sub, add
     from sys import argv
     operator = ["+", "-", "/", "*"]
+    operator2 = ["/", "-"]
+    operator3 = ["+", "*"]
     count = len(argv) - 1
     sign = argv[2]
     if count != 3:
@@ -11,17 +13,17 @@ if __name__ == "__main__":
     if sign not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
     a = int(argv[1])
     b = int(argv[3])
     res = 0
-    if sign in operator:
+    if sign in operator3:
         if sign == "+":
             res = add(a, b)
-        elif sign == "-":
-            res = sub(a, b)
-        elif sign == "*":
+        else:
             res = mul(a, b)
+    if sign in operator2:
+        if sign == "-":
+            res = sub(a, b)
         else:
             res = div(a, b)
     print("{} {} {} = {}".format(a, sign, b, res))
