52dbc9d5863f15eb5f4f8e148f9787c72ac27aba
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-23 15:13:16 +0100

    py files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     operator2 = ["/", "-"]
     operator3 = ["+", "*"]
     count = len(argv) - 1
-    sign = argv[2]
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
         exit(1)
+    sign = argv[2]
     if sign not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
