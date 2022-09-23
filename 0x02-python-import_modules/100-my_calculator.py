d8f302c9a1f306122c5dfff9946f5aaa6278a21b
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-23 15:46:13 +0100

    py files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     if sys.argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
