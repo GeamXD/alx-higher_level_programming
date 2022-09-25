8395e0bf118c23a4e9155823d27e12fbd59fc11c
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-25 17:10:13 +0100

    py files

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for val in my_list:
-        print("{:d}".format(int(val)))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
