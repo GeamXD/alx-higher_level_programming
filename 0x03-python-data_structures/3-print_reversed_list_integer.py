8452389ec2ab4b6a4da3a9737900a2fe49b91f71
Musa Godwin <musa.godwin8112@gmail.com>
2022-09-25 18:30:41 +0100

    py files

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for val in my_list:
-        print("{:d}".format(val))
+    if my_list:
+        my_list.reverse()
+        for val in my_list:
+            print("{:d}".format(val))
