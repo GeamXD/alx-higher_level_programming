ebb6cf90fdf6ed4957c015f21f50ce937f21424f
Musa Godwin <musa.godwin8112@gmail.com>
2022-10-11 19:47:23 +0100

    py files

---
+++ 0x05-python-exceptions/ggs/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for val in range(list_length):
+        try:
+            res = my_list_1[val] / my_list_2[val]
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        except TypeError:
+            print("wrong type")
+            res = 0
+        finally:
+            new_list.append(res)
+    return new_list
+
+
